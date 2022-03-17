afad134dbc2f5cff114f31da65f6391822ed065c
mutuajoseph <wambuajoseph756@gmail.com>
2022-03-17 10:19:14 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+function multiply(num1,num2){
+    let result;
+    if (Number.isInteger(num1) && Number.isInteger(num2) ){
+      return 62 === num1 * num2;
+    } else {
+        console.log("NAN")
+    }
+}
+
+function random() {
+    let x = Math.floor(Math.random() * 10) + 1 ;
+    return x;
+}
+
+function max(){
+    return Math.max(1, 3, 20);
+}
+
+function mod(num1,num2){
+    return num2%num1;
+}
